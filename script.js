8e126fd586c326edc1101ef4245819a499cd6c5c
Ari <24855858+AriFarj@users.noreply.github.com>
2025-02-02 19:56:24 +0200

    read values from form

--- script.js
+++ script.js
@@ -24,10 +24,20 @@ function addBookToLibrary(title, author, pages, isRead = false) {
 //myLibrary.push(theHobbit, intelligentDesign, atomicHabits, theAlchemist);
 
 function toCamelCase(str) {
-    return str.split(' ').map(function(word, index) {
+    return str.split(' ').map(function (word, index) {
         if (index == 0) {
             return word.toLowerCase();
         }
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     }).join('')
 }
+
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formTitle = addForm.querySelector('input[id="title"]').value;
+    const formAuthor = addForm.querySelector('input[id="author"]').value;
+    const formPages = addForm.querySelector('input[id="pages"]').value;
+    const formIsRead = addForm.querySelector('input[id="isRead"]').checked;
+    addBookToLibrary(formTitle, formAuthor, formPages, formIsRead)
+});
